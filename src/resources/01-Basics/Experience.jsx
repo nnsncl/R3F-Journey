69ce9886f1f8cf174f8171f8259658322a6536a0
nnsncl <nuniroland@gmail.com>
2023-05-06 16:45:43 +0200

    Add a plane as floor, position meshes

--- src/resources/01-Basics/Experience.jsx
+++ src/resources/01-Basics/Experience.jsx
@@ -4,15 +4,18 @@ import { Canvas } from "@react-three/fiber";
 export const Experience = () => {
     return (
         <React.Fragment>
+            <mesh position-x={-2}>
+                <sphereGeometry />
+                <meshBasicMaterial color='orange' />
+            </mesh>
             <mesh scale={1.5} position-x={2} rotation-y={Math.PI * 0.25} >
                 <boxGeometry scale={1.5} />
                 {/* <boxGeometry args={[1.5, 32, 32]} /> */}
                 <meshBasicMaterial color='mediumPurple' />
             </mesh>
-            <mesh position-x={-2}>
-                <boxGeometry />
-                {/* <boxGeometry args={[1.5, 32, 32]} /> */}
-                <meshBasicMaterial color='orange' />
+            <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+                <planeGeometry />
+                <meshBasicMaterial color='greenyellow' />
             </mesh>
         </React.Fragment>
     );
