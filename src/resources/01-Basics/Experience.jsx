da307ecb4c2615437a93dc9ed46d5cce8bdf3286
nnsncl <nuniroland@gmail.com>
2023-05-06 20:33:22 +0200

    Handle orbitControls (the wrong/hard way)

--- src/resources/01-Basics/Experience.jsx
+++ src/resources/01-Basics/Experience.jsx
@@ -1,19 +1,25 @@
 import React, { useRef } from "react";
-import { useFrame } from "@react-three/fiber";
+import { extend, useFrame, useThree } from "@react-three/fiber";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+
+extend({ OrbitControls });
 
 export const Experience = () => {
-    const cubeRef = useRef()
-    const groupRef = useRef()
-    // Called on each frame before rendering the scene
-    useFrame((state, delta) => {
-        groupRef.current.rotation.y += delta;
-        cubeRef.current.rotation.x += delta;
-    })
+    const cubeRef = useRef();
+    const groupRef = useRef();
+    const { camera, gl } = useThree(); // Called a single time on scene load
 
+    // Called on each frame before rendering the scene
+    useFrame((_state, delta) => {
+        // groupRef.current.rotation.y += delta;
+        cubeRef.current.rotation.y += delta;
+    });
 
 
     return (
         <React.Fragment>
+            <orbitControls args={[camera, gl.domElement]} />
             <group ref={groupRef}>
                 <mesh position-x={-2}>
                     <sphereGeometry />
@@ -25,6 +31,7 @@ export const Experience = () => {
                     <meshBasicMaterial color='mediumPurple' />
                 </mesh>
             </group>
+
             <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
                 <planeGeometry />
                 <meshBasicMaterial color='greenyellow' />
