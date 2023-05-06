3dcaf89b1fde43f8685756416b1ae09b4d556529
nnsncl <nuniroland@gmail.com>
2023-05-06 20:40:51 +0200

    handle lights

--- src/resources/01-Basics/Experience.jsx
+++ src/resources/01-Basics/Experience.jsx
@@ -16,25 +16,26 @@ export const Experience = () => {
         cubeRef.current.rotation.y += delta;
     });
 
-
     return (
         <React.Fragment>
             <orbitControls args={[camera, gl.domElement]} />
+            <directionalLight position={[1, 2, 3]} intensity={1.5} />
+            <ambientLight intensity={0.5} />
             <group ref={groupRef}>
                 <mesh position-x={-2}>
                     <sphereGeometry />
-                    <meshBasicMaterial color='orange' />
+                    <meshStandardMaterial color='orange' />
                 </mesh>
                 <mesh ref={cubeRef} scale={1.5} position-x={2} rotation-y={Math.PI * 0.25} >
                     <boxGeometry scale={1.5} />
                     {/* <boxGeometry args={[1.5, 32, 32]} /> */}
-                    <meshBasicMaterial color='mediumPurple' />
+                    <meshStandardMaterial color='mediumPurple' />
                 </mesh>
             </group>
 
             <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
                 <planeGeometry />
-                <meshBasicMaterial color='greenyellow' />
+                <meshStandardMaterial color='greenyellow' />
             </mesh>
         </React.Fragment>
     );
