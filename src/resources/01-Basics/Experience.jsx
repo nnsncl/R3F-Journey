a230b88ebf3e60d98c6b5dd15323af1708476ee9
nnsncl <nuniroland@gmail.com>
2023-05-06 16:54:35 +0200

    Animate groups

--- src/resources/01-Basics/Experience.jsx
+++ src/resources/01-Basics/Experience.jsx
@@ -3,23 +3,28 @@ import { useFrame } from "@react-three/fiber";
 
 export const Experience = () => {
     const cubeRef = useRef()
+    const groupRef = useRef()
     // Called on each frame before rendering the scene
     useFrame((state, delta) => {
-
-        cubeRef.current.rotation.y += delta;
+        groupRef.current.rotation.y += delta;
+        cubeRef.current.rotation.x += delta;
     })
+
+
 
     return (
         <React.Fragment>
-            <mesh position-x={-2}>
-                <sphereGeometry />
-                <meshBasicMaterial color='orange' />
-            </mesh>
-            <mesh ref={cubeRef} scale={1.5} position-x={2} rotation-y={Math.PI * 0.25} >
-                <boxGeometry scale={1.5} />
-                {/* <boxGeometry args={[1.5, 32, 32]} /> */}
-                <meshBasicMaterial color='mediumPurple' />
-            </mesh>
+            <group ref={groupRef}>
+                <mesh position-x={-2}>
+                    <sphereGeometry />
+                    <meshBasicMaterial color='orange' />
+                </mesh>
+                <mesh ref={cubeRef} scale={1.5} position-x={2} rotation-y={Math.PI * 0.25} >
+                    <boxGeometry scale={1.5} />
+                    {/* <boxGeometry args={[1.5, 32, 32]} /> */}
+                    <meshBasicMaterial color='mediumPurple' />
+                </mesh>
+            </group>
             <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
                 <planeGeometry />
                 <meshBasicMaterial color='greenyellow' />
