b872fc877f7d4a0de66614acc277cae3921bdd1a
nnsncl <nuniroland@gmail.com>
2023-05-06 23:37:31 +0200

    Add TransformControls

--- src/resources/02-Drei/Experience.jsx
+++ src/resources/02-Drei/Experience.jsx
@@ -1,25 +1,30 @@
 import React from "react";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, TransformControls } from "@react-three/drei";
 
 export const Experience = () => {
+    const gizmoTargRef = React.useRef();
 
     return (
         <React.Fragment>
-            <OrbitControls />
+            <OrbitControls makeDefault />
             <directionalLight position={[1, 2, 3]} intensity={1.5} />
             <ambientLight intensity={0.5} />
             <mesh position-x={-2}>
                 <sphereGeometry />
                 <meshStandardMaterial color="orange" />
             </mesh>
-            <mesh position-x={2} scale={1.5}>
+
+            <TransformControls object={gizmoTargRef} mode="translate" />
+            <mesh ref={gizmoTargRef} position-x={2} scale={1.5}>
                 <boxGeometry />
                 <meshStandardMaterial color="mediumpurple" />
             </mesh>
+
             <mesh position-y={-1} rotation-x={- Math.PI * 0.5} scale={10}>
                 <planeGeometry />
                 <meshStandardMaterial color="greenyellow" />
             </mesh>
+
         </React.Fragment>
     );
 };
