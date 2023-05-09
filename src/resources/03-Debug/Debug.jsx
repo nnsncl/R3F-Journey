a0f7161f6a99aaea05851c387f38fd5d7c4bdbbe
nnsncl <nuniroland@gmail.com>
2023-05-09 23:19:48 +0200

    Add Leva component

--- src/resources/03-Debug/Debug.jsx
+++ src/resources/03-Debug/Debug.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { Experience } from './Experience';
-
+import { Leva } from "leva";
 export const Debug = () => {
     return (
-        <Canvas
-            camera={{
-                fov: 45,
-                near: 0.1,
-                far: 200,
-                position: [- 4, 3, 6]
-            }}
-        >
-            <Experience />
-        </Canvas>
+        <React.Fragment>
+            <Leva collapsed />
+            <Canvas
+                camera={{
+                    fov: 45,
+                    near: 0.1,
+                    far: 200,
+                    position: [- 4, 3, 6]
+                }}
+            >
+                <Experience />
+            </Canvas>
+        </React.Fragment>
+
     );
 };
