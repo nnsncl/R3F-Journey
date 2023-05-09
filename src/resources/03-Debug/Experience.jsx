2efef9c8c54b8c796e8cc41b80bd96935a913305
nnsncl <nuniroland@gmail.com>
2023-05-09 22:53:37 +0200

    Add color controls

--- src/resources/03-Debug/Experience.jsx
+++ src/resources/03-Debug/Experience.jsx
@@ -8,6 +8,9 @@ export const Experience = () => {
         directional,
         cubePositionX,
         spherePositionX,
+        sphereColor,
+        cubeColor,
+        floorColor,
     } = useControls({
         ambiant: {
             value: 0.5,
@@ -33,6 +36,9 @@ export const Experience = () => {
             max: 10,
             step: 0.1,
         },
+        sphereColor: 'orange',
+        cubeColor: 'mediumpurple',
+        floorColor: 'greenyellow'
     })
 
     return (
@@ -43,17 +49,17 @@ export const Experience = () => {
 
             <mesh position-x={spherePositionX} >
                 <sphereGeometry />
-                <meshStandardMaterial color="orange" />
+                <meshStandardMaterial color={sphereColor} />
             </mesh>
 
             <mesh position-x={cubePositionX} scale={1.5} >
                 <boxGeometry />
-                <meshStandardMaterial color="mediumpurple" />
+                <meshStandardMaterial color={cubeColor} />
             </mesh>
 
             <mesh position-y={-1} rotation-x={- Math.PI * 0.5} scale={10}>
                 <planeGeometry />
-                <meshStandardMaterial color='greenyellow' />
+                <meshStandardMaterial color={floorColor} />
             </mesh>
         </React.Fragment>
     );
