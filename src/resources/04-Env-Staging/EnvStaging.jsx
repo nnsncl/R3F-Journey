19b30fd9f799a6a7478f59a572c09bc62f7da951
nnsncl <nuniroland@gmail.com>
2023-05-10 21:18:19 +0200

    Apply background color to the scene

--- src/resources/04-Env-Staging/EnvStaging.jsx
+++ src/resources/04-Env-Staging/EnvStaging.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { Experience } from './Experience';
 import { Leva } from "leva";
+import * as THREE from 'three';
+
 export const EnvStaging = () => {
+
+    // const gl_created = ({ gl, scene }) => {
+    //     // Set the background color into the canvas
+    //     // gl.setClearColor('#212121', 1);
+    //     // scene.background = new THREE.Color('#212121');
+    // }
+
     return (
         <React.Fragment>
             <Leva collapsed />
             <Canvas
+                // onCreated={gl_created}
                 camera={{
                     fov: 45,
                     near: 0.1,
@@ -14,6 +24,7 @@ export const EnvStaging = () => {
                     position: [- 4, 3, 6]
                 }}
             >
+                <color args={['#212121']} attach='background' />
                 <Experience />
             </Canvas>
         </React.Fragment>
