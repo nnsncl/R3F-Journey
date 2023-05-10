9827073652427362877b78b4ec3bdda83c299136
nnsncl <nuniroland@gmail.com>
2023-05-10 21:07:06 +0200

    Extract debug controls to an isolated component

---
+++ src/resources/04-Env-Staging/Debug.jsx
@@ -0,0 +1,57 @@
+import { useControls } from "leva";
+
+export const DebugControls = () => {
+    const { showPerfs } = useControls('perfs', {
+        showPerfs: false,
+    })
+    const { ambiant, directional } = useControls('lights', {
+        ambiant: {
+            value: 0.5,
+            min: 0,
+            max: 10,
+            step: 0.1,
+        },
+        directional: {
+            value: 1.5,
+            min: 0,
+            max: 10,
+            step: 0.1,
+        }
+    })
+    const { spherePosition, sphereColor, sphereVisible } = useControls('sphere', {
+        sphereVisible: true,
+        spherePosition: {
+            value: { x: 2, y: 0 },
+            joystick: 'invertY',
+            min: -10,
+            max: 10,
+            step: 0.1,
+        },
+        sphereColor: 'orange',
+    })
+    const { cubePosition, cubeColor } = useControls('cube', {
+        cubePosition: {
+            value: { x: -2, y: 0 },
+            joystick: 'invertY',
+            min: -10,
+            max: 10,
+            step: 0.1,
+        },
+        cubeColor: 'mediumpurple',
+    })
+    const { floorColor } = useControls('floor', {
+        floorColor: 'greenyellow',
+    })
+
+    return {
+        showPerfs,
+        ambiant,
+        directional,
+        spherePosition,
+        sphereColor,
+        sphereVisible,
+        cubePosition,
+        cubeColor,
+        floorColor,
+    }
+}
