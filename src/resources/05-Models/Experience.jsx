9adc8a362a769deb94ceaba7343311c37cd0bb96
nnsncl <nuniroland@gmail.com>
2023-05-13 16:14:14 +0200

    Lazy load heavy models with suspense

--- src/resources/05-Models/Experience.jsx
+++ src/resources/05-Models/Experience.jsx
@@ -6,13 +6,20 @@ import { OrbitControls } from "@react-three/drei";
 import { useControls } from "leva";
 import { Perf } from "r3f-perf";
 
-export const Experience = () => {
-    const { showPerfs } = useControls('perfs', { showPerfs: false })
-    const model = useLoader(GLTFLoader, './models/hamburger-draco.glb', (loader) => {
+const Model = () => {
+    const model = useLoader(GLTFLoader, './models/FlightHelmet/glTF/FlightHelmet.gltf', (loader) => {
         const draco = new DRACOLoader();
         draco.setDecoderPath('./models/draco/');
         loader.setDRACOLoader(draco);
     })
+
+    return <primitive object={model.scene} scale={3} position-y={-1} />
+}
+
+
+export const Experience = () => {
+    const { showPerfs } = useControls('perfs', { showPerfs: false })
+
     return (
         <React.Fragment>
             {showPerfs && (
@@ -23,13 +30,13 @@ export const Experience = () => {
             <directionalLight position={[1, 2, 3]} intensity={1.5} />
             <ambientLight intensity={0.5} />
 
-
-            <primitive object={model.scene} scale={0.1} />
-
+            <React.Suspense>
+                <Model />
+            </React.Suspense>
 
             <mesh position-y={-1} rotation-x={- Math.PI * 0.5} scale={10}>
                 <planeGeometry />
-                <meshStandardMaterial color="#FFFFFF" />
+                <meshStandardMaterial color="greenyellow" />
             </mesh>
         </React.Fragment>
     );
