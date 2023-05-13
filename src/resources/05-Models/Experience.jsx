8a0f3151f3e6dd66e7c7e66e4a9bbe8b8351691a
nnsncl <nuniroland@gmail.com>
2023-05-13 16:27:06 +0200

    Create a Cube loader component with suspense

--- src/resources/05-Models/Experience.jsx
+++ src/resources/05-Models/Experience.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
-import { OrbitControls } from "@react-three/drei";
+import { Float, OrbitControls } from "@react-three/drei";
 import { useControls } from "leva";
 import { Perf } from "r3f-perf";
+import { MeshStandardMaterial } from "three";
 
 const Model = () => {
     const model = useLoader(GLTFLoader, './models/FlightHelmet/glTF/FlightHelmet.gltf', (loader) => {
@@ -13,9 +14,16 @@ const Model = () => {
         loader.setDRACOLoader(draco);
     })
 
-    return <primitive object={model.scene} scale={3} position-y={-1} />
+    return <primitive object={model.scene} scale={5} position-y={-1} />
 }
-
+const MeshLoader = (props) => {
+    return (
+        <mesh {...props} >
+            <boxGeometry args={[1, 1, 1, 2, 2, 2]} />
+            <meshBasicMaterial wireframe color='red' />
+        </mesh>
+    )
+}
 
 export const Experience = () => {
     const { showPerfs } = useControls('perfs', { showPerfs: false })
@@ -30,7 +38,7 @@ export const Experience = () => {
             <directionalLight position={[1, 2, 3]} intensity={1.5} />
             <ambientLight intensity={0.5} />
 
-            <React.Suspense>
+            <React.Suspense fallback={<MeshLoader position-y={0.5} scale={[2, 3, 2]} />} >
                 <Model />
             </React.Suspense>
 
