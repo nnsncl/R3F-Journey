bb96e558f2ef8bf3f945ff8bb246edd92c1fc3d9
nnsncl <nuniroland@gmail.com>
2023-05-14 17:30:40 +0200

    Decompose model nodes into jsx

--- src/resources/07-Portal/Experience.jsx
+++ src/resources/07-Portal/Experience.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { OrbitControls } from "@react-three/drei";
+import { useGLTF, useTexture, OrbitControls, Center } from "@react-three/drei";
 import { useControls } from "leva";
 import { Perf } from "r3f-perf";
 
-export const Experience = () => {
-    const { showPerfs } = useControls('perfs', {
-        showPerfs: false,
-    })
+const Commons = () => {
+    const { showPerfs } = useControls('perfs', { showPerfs: false })
 
     return (
         <React.Fragment>
@@ -14,15 +12,51 @@ export const Experience = () => {
                 <Perf position="bottom-left" visible={showPerfs} />
             )}
 
-            <OrbitControls makeDefault />
             <directionalLight position={[1, 2, 3]} intensity={1.5} />
             <ambientLight intensity={0.5} />
+        </React.Fragment>
+    )
+}
 
-            <mesh position={[0, 1, 0]} >
-                <sphereGeometry />
-                <meshNormalMaterial />
-            </mesh>
+export const Experience = () => {
+    const { nodes } = useGLTF('./models/portal/portal.glb')
+    const bakedTexture = useTexture('./models/portal/baked.jpg')
 
+    console.log(nodes)
+    return (
+        <React.Fragment>
+            <color args={['#212121']} attach='background' />
+            <Commons />
+            <OrbitControls makeDefault />
+
+            <Center>
+                <mesh geometry={nodes.baked.geometry} >
+                    <meshBasicMaterial
+                        map={bakedTexture}
+                        map-flipY={false}
+                    />
+                </mesh>
+
+                <mesh
+                    geometry={nodes.poleLightA.geometry}
+                    position={nodes.poleLightA.position}
+                >
+                    <meshBasicMaterial color='ivory' />
+                </mesh>
+                <mesh
+                    geometry={nodes.poleLightB.geometry}
+                    position={nodes.poleLightB.position}
+                >
+                    <meshBasicMaterial color='ivory' />
+                </mesh>
+                <mesh
+                    geometry={nodes.portalLight.geometry}
+                    position={nodes.portalLight.position}
+                    rotation={nodes.portalLight.rotation}
+                >
+                    <meshBasicMaterial color='red' />
+                </mesh>
+            </Center>
         </React.Fragment>
     );
 };
