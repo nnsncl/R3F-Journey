92bc85de2456439d70bf32abfa38cbb66c826f73
nnsncl <nuniroland@gmail.com>
2023-05-16 23:06:26 +0200

    Randomize cube color onclick

--- src/resources/08-MouseEvents/Experience.jsx
+++ src/resources/08-MouseEvents/Experience.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { useFrame } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { useControls } from "leva";
 import { Perf } from "r3f-perf";
 
 export const Experience = () => {
-    const { showPerfs } = useControls('perfs', {
-        showPerfs: false,
-    })
+    const { showPerfs } = useControls('perfs', { showPerfs: false })
+    const cubeRef = React.useRef()
 
+    const eventHandler = () => {
+        cubeRef.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 75%)`)
+    }
+
+    useFrame((_, delta) => {
+        cubeRef.current.rotation.y += delta
+    })
     return (
         <React.Fragment>
             {showPerfs && (
@@ -18,8 +25,8 @@ export const Experience = () => {
             <directionalLight position={[1, 2, 3]} intensity={1.5} />
             <ambientLight intensity={0.5} />
 
-            <mesh position={[0, 0, 0]} >
-                <sphereGeometry />
+            <mesh ref={cubeRef} position={[0, 0, 0]} onClick={eventHandler} >
+                <boxGeometry />
                 <meshStandardMaterial color="ivory" />
             </mesh>
         </React.Fragment>
