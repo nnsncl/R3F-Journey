1bfe5021c4582fdbbbb96ab668d1ae44a86df452
nnsncl <nuniroland@gmail.com>
2023-05-18 17:05:58 +0200

    Events on complex objects

--- src/resources/08-MouseEvents/Experience.jsx
+++ src/resources/08-MouseEvents/Experience.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { useFrame } from "@react-three/fiber";
-import { OrbitControls, Sparkles } from "@react-three/drei";
+import { OrbitControls, Sparkles, meshBounds, useGLTF } from "@react-three/drei";
 import { useControls } from "leva";
 import { Perf } from "r3f-perf";
 
 export const Experience = () => {
     const { showPerfs } = useControls('perfs', { showPerfs: false })
-    const cubeRef = React.useRef()
     const [showSparkles, setShowSparkles] = React.useState(false)
-    const eventHandler = () => {
+    const model = useGLTF('./models/hamburger.glb')
+    const cubeRef = React.useRef()
+
+    const eventHandler = () =>
         cubeRef.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 75%)`)
-    }
-    const pointerEventHandler = () =>
-        !showSparkles && setShowSparkles(!showSparkles)
+    // const pointerEventHandler = () =>
+    //     !showSparkles && setShowSparkles(!showSparkles)
 
     useFrame((_, delta) => {
         cubeRef.current.rotation.y += delta
@@ -28,7 +29,6 @@ export const Experience = () => {
             <ambientLight intensity={0.5} />
 
             <mesh
-                ref={cubeRef}
                 scale={[1, 1, 1]}
                 position={[2, 0, 0]}
                 onClick={(event) => event.stopPropagation()}
@@ -36,13 +36,28 @@ export const Experience = () => {
                 <boxGeometry />
                 <meshStandardMaterial color="red" />
             </mesh>
+
+            <primitive
+                {...model}
+                object={model.scene}
+                position={[1, 1, 0]}
+                scale={0.1}
+                onClick={(event) => {
+                    event.stopPropagation()
+                    console.log(event.object.name)
+                }}
+            />
+
             <mesh
                 ref={cubeRef}
                 scale={[1, 1.5, 1]}
                 position={[0, 0, 0]}
+                raycast={meshBounds}
                 onClick={eventHandler}
                 onContextMenu={() => setShowSparkles(!showSparkles)}
-                onPointerOver={pointerEventHandler}
+                // onPointerOver={pointerEventHandler}
+                onPointerEnter={() => document.body.style.cursor = 'pointer'}
+                onPointerLeave={() => document.body.style.cursor = 'default'}
             >
                 <octahedronGeometry />
                 <meshStandardMaterial color="ivory" />
@@ -64,3 +79,4 @@ export const Experience = () => {
         </React.Fragment>
     );
 };
+useGLTF.preload("./models/hamburger.glb");
