54d117d9762b70c099083a82a2d9bbac764073af
nnsncl <nuniroland@gmail.com>
2023-05-20 14:56:11 +0200

    Apply rigi bodies, add debug UI

--- src/resources/11-Physics/Experience.jsx
+++ src/resources/11-Physics/Experience.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { useControls } from 'leva'
 import { Perf } from 'r3f-perf'
 import { OrbitControls } from '@react-three/drei'
-
+import { Debug, Physics, RigidBody } from '@react-three/rapier'
 
 export const Experience = () => {
-    const { showPerfs } = useControls('perfs', {
+    const { showPerfs, showDebug } = useControls('Monitoring', {
         showPerfs: false,
+        showDebug: true,
     })
 
     return (
@@ -17,18 +18,30 @@ export const Experience = () => {
             <OrbitControls makeDefault />
             <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
             <ambientLight intensity={0.5} />
-            <mesh castShadow position={[- 2, 2, 0]}>
-                <sphereGeometry />
-                <meshStandardMaterial color="mediumpurple" />
-            </mesh>
-            <mesh castShadow position={[2, 2, 0]}>
-                <boxGeometry />
-                <meshStandardMaterial color="tomato" />
-            </mesh>
-            <mesh receiveShadow position-y={- 1.25}>
-                <boxGeometry args={[10, 0.5, 10]} />
-                <meshStandardMaterial color="ivory" />
-            </mesh>
+
+            <Physics>
+                {showDebug && <Debug />}
+                <RigidBody>
+                    <mesh castShadow position={[- 2, 2, 0]}>
+                        <sphereGeometry />
+                        <meshStandardMaterial color="mediumpurple" />
+                    </mesh>
+                </RigidBody>
+                <mesh castShadow position={[2, 2, 0]}>
+                    <boxGeometry />
+                    <meshStandardMaterial color="tomato" />
+                </mesh>
+
+                <RigidBody type='fixed'>
+                    <mesh receiveShadow position-y={- 1.25}>
+                        <boxGeometry args={[10, 0.5, 10]} />
+                        <meshStandardMaterial color="ivory" />
+                    </mesh>
+                </RigidBody>
+
+            </Physics>
+
+
         </React.Fragment>
     )
 }
