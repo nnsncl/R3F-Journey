756414fb6214c7dab9f485c6cf4be86a8adf4c1c
nnsncl <nuniroland@gmail.com>
2023-05-22 22:13:52 +0200

    Add interface component to the Ui

--- src/resources/12-Game/index.jsx
+++ src/resources/12-Game/index.jsx
@@ -1,33 +1,36 @@
 import React from "react";
+import { Leva } from "leva";
 import { Canvas } from "@react-three/fiber";
 import { KeyboardControls } from "@react-three/drei";
 import { Experience } from './Experience';
-import { Leva } from "leva";
+import { Interface } from "./Interface";
 
 export const Game = () => {
     return (
-        <KeyboardControls
-            map={[
-                { name: 'forward', keys: ['ArrowUp', 'KeyW'] },
-                { name: 'backward', keys: ['ArrowDown', 'KeyS'] },
-                { name: 'leftward', keys: ['ArrowLeft', 'KeyA'] },
-                { name: 'rightward', keys: ['ArrowRight', 'KeyD'] },
-                { name: 'jump', keys: ['Space'] }
-            ]}
-        >
-            <Leva collapsed />
-            <Canvas
-                shadows
-                camera={{
-                    fov: 45,
-                    near: 0.1,
-                    far: 200,
-                    position: [- 4, 3, 6]
-                }}
+        <React.Fragment>
+            <KeyboardControls
+                map={[
+                    { name: 'forward', keys: ['ArrowUp', 'KeyW'] },
+                    { name: 'backward', keys: ['ArrowDown', 'KeyS'] },
+                    { name: 'leftward', keys: ['ArrowLeft', 'KeyA'] },
+                    { name: 'rightward', keys: ['ArrowRight', 'KeyD'] },
+                    { name: 'jump', keys: ['Space'] }
+                ]}
             >
-                <Experience />
-            </Canvas>
-        </KeyboardControls>
-
+                <Leva collapsed />
+                <Canvas
+                    shadows
+                    camera={{
+                        fov: 45,
+                        near: 0.1,
+                        far: 200,
+                        position: [- 4, 3, 6]
+                    }}
+                >
+                    <Experience />
+                </Canvas>
+                <Interface />
+            </KeyboardControls>
+        </React.Fragment>
     );
 };
