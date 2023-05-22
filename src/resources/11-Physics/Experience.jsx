552a2b4176b61ec81979e2e1ab3279ba3706fe23
nnsncl <nuniroland@gmail.com>
2023-05-22 12:20:33 +0200

    Instanciate multiple cube, add randomness to interactions, automate interactions

--- src/resources/11-Physics/Experience.jsx
+++ src/resources/11-Physics/Experience.jsx
@@ -9,21 +9,48 @@ import {
     RigidBody,
     BallCollider,
     CuboidCollider,
-    CylinderCollider
+    CylinderCollider,
+    InstancedRigidBodies
 } from '@react-three/rapier'
 import { useFrame } from '@react-three/fiber'
 
 export const Experience = () => {
-    const { showPerfs, showDebug } = useControls('Monitoring', {
+    const { showPerfs, showDebug, stressTestAmount } = useControls('Monitoring', {
         showPerfs: false,
-        showDebug: true,
+        showDebug: false,
+        stressTestAmount: 300
     })
     const model = useGLTF('./models/hamburger.glb')
 
     const [hitSound] = React.useState(new Audio('./sounds/hit.mp3'))
+    const cubeTransforms = React.useMemo(() => {
+        const positions = []
+        const rotations = []
+        const scales = []
+
+        for (let i = 0; i < stressTestAmount; i++) {
+            positions.push([
+                Math.random() - 0.5 * 8,
+                6 + i + 0.2,
+                Math.random() - 0.5 * 8
+            ])
+            rotations.push([
+                Math.random(),
+                Math.random(),
+                Math.random()
+            ])
+
+            const scale = 0.2 + Math.random() * 0.8
+            scales.push([scale, scale, scale])
+        }
+
+        return { positions, rotations, scales }
+    }, [])
+
+
     const impulseRef = React.useRef()
     const carousselRef = React.useRef()
-
+    const stressTestRef = React.useRef()
 
     const createImpulse = () => {
         const mass = impulseRef.current.mass()
@@ -44,6 +71,19 @@ export const Experience = () => {
         // hitSound.volume = Math.random()
         // hitSound.play()
     }
+
+    React.useEffect(() => {
+        for (let i = 0; i < stressTestAmount; i++) {
+            const matrix = new THREE.Matrix4()
+            matrix.compose(
+                new THREE.Vector3(i * 2, 0, 0),
+                new THREE.Quaternion(),
+                new THREE.Vector3(1, 1, 1)
+            )
+
+            stressTestRef.current.setMatrixAt(i, matrix)
+        }
+    }, [])
 
     useFrame((state, delta) => {
         const time = state.clock.getElapsedTime()
@@ -112,18 +152,34 @@ export const Experience = () => {
 
                 <RigidBody position={[0, 4, 0]}  >
                     {/* <CylinderCollider args={[0.5, 1.25]} /> */}
-                    <primitive object={model.scene} scale={0.25} />
+                    <primitive object={model.scene} scale={0.1} />
                 </RigidBody>
 
-                <RigidBody
-                    type='fixed'
-                    friction={0.7}
-                >
+                <RigidBody type='fixed' friction={0.7}>
                     <mesh receiveShadow position-y={- 1.25}>
                         <boxGeometry args={[10, 0.5, 10]} />
                         <meshStandardMaterial color="ivory" />
                     </mesh>
                 </RigidBody>
+
+                <RigidBody type='fixed'>
+                    <CuboidCollider args={[5, 2, 0.5]} position={[0, 1, 5.5]} />
+                    <CuboidCollider args={[5, 2, 0.5]} position={[0, 1, -5.5]} />
+                    <CuboidCollider args={[5, 2, 0.5]} position={[5.5, 1, 0]} rotation={[0, Math.PI * 0.5, 0]} />
+                    <CuboidCollider args={[5, 2, 0.5]} position={[-5.5, 1, 0]} rotation={[0, Math.PI * 0.5, 0]} />
+                </RigidBody>
+
+                <InstancedRigidBodies
+                    positions={cubeTransforms.positions}
+                    rotations={cubeTransforms.rotations}
+                    scales={cubeTransforms.scales}
+                >
+                    <instancedMesh castShadow ref={stressTestRef} args={[null, null, stressTestAmount]}>
+                        <boxGeometry />
+                        <meshStandardMaterial color="tomato" />
+                    </instancedMesh>
+                </InstancedRigidBodies>
+
 
                 {/* <RigidBody colliders='ball' >
                     <mesh castShadow position={[- 2, 2, 0]}>
