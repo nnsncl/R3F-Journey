b2a00dfcbb53865d7aca7b7f8264f36c351c770c
nnsncl <nuniroland@gmail.com>
2023-05-22 21:30:25 +0200

    Animate lights and shadows

--- src/resources/12-Game/Lights.jsx
+++ src/resources/12-Game/Lights.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { useFrame } from "@react-three/fiber";
 
 export const Lights = () => {
+    const dirLightRef = React.useRef()
+
+    useFrame((state) => {
+        dirLightRef.current.position.z = state.camera.position.z + 1 - 4
+        dirLightRef.current.target.position.z = state.camera.position.z - 4
+        dirLightRef.current.target.updateMatrixWorld()
+    })
     return (
         <React.Fragment>
-            <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+            <directionalLight
+                ref={dirLightRef}
+                castShadow
+                position={[4, 4, 1]}
+                intensity={1.5}
+                shadow-mapSize={[1024, 1024]}
+                shadow-camera-near={1}
+                shadow-camera-far={10}
+                shadow-camera-top={10}
+                shadow-camera-right={10}
+                shadow-camera-bottom={-10}
+                shadow-camera-left={-10}
+            />
             <ambientLight intensity={0.5} />
         </React.Fragment>
     );
