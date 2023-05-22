1291e1146455c18a804ad8fa3fdeab61cd2e1b21
nnsncl <nuniroland@gmail.com>
2023-05-22 16:21:15 +0200

    Instanciate KeyboardControls

--- src/resources/12-Game/index.jsx
+++ src/resources/12-Game/index.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
+import { KeyboardControls } from "@react-three/drei";
 import { Experience } from './Experience';
 import { Leva } from "leva";
 
 export const Game = () => {
     return (
-        <React.Fragment>
+        <KeyboardControls
+            map={[
+                { name: 'forward', keys: ['ArrowUp', 'KeyW'] },
+                { name: 'backward', keys: ['ArrowDown', 'KeyS'] },
+                { name: 'leftward', keys: ['ArrowLeft', 'KeyA'] },
+                { name: 'rightward', keys: ['ArrowRight', 'KeyD'] },
+                { name: 'jump', keys: ['Space'] }
+            ]}
+        >
             <Leva collapsed />
             <Canvas
                 shadows
@@ -18,7 +27,7 @@ export const Game = () => {
             >
                 <Experience />
             </Canvas>
-        </React.Fragment>
+        </KeyboardControls>
 
     );
 };
