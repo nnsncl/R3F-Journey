2d76cc8f0e0158584d6c358f1de8140ee94f11cb
nnsncl <nuniroland@gmail.com>
2023-05-22 12:47:55 +0200

    Optimize geometries draw calls

--- src/resources/12-Game/Level.jsx
+++ src/resources/12-Game/Level.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React from "react"
+import * as THREE from 'three'
+
+
+THREE.ColorManagement.legacyMode = false
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+const materials = {
+    floor01: new THREE.MeshStandardMaterial({ color: 'limegreen' }),
+    floor02: new THREE.MeshStandardMaterial({ color: 'greenyellow' }),
+    floor03: new THREE.MeshStandardMaterial({ color: 'orangered' }),
+    wall: new THREE.MeshStandardMaterial({ color: 'slategrey' }),
+}
 
 const BlockStart = ({ position = [0, 0, 0] }) => {
+
     return (
         <group position={position} >
-            <mesh receiveShadow position={[0, -0.1, 0]} >
-                <boxGeometry args={[4, 0.2, 4]} />
-                <meshStandardMaterial color='ivory' />
-            </mesh>
+            {/* Floor */}
+            <mesh
+                receiveShadow
+                geometry={boxGeometry}
+                material={materials.floor01}
+                position={[0, -0.1, 0]}
+                scale={[4, 0.2, 4]}
+            />
         </group>
     )
 }
