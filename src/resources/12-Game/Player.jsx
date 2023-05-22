26c048be092d1c9f4aa8264ba2a5341b5384d488
nnsncl <nuniroland@gmail.com>
2023-05-22 16:40:05 +0200

    Add key controls basics

--- src/resources/12-Game/Player.jsx
+++ src/resources/12-Game/Player.jsx
@@ -1,9 +1,44 @@
 import React from "react"
 import { RigidBody } from "@react-three/rapier"
+import { useFrame } from "@react-three/fiber"
+import { useKeyboardControls } from "@react-three/drei"
 
 export const Player = () => {
+    const [subscribedKeys, getKeys] = useKeyboardControls()
+
+    const bodyRef = React.useRef()
+    useFrame((state, delta) => {
+        const { forward, backward, leftward, rightward } = getKeys()
+        const impulse = { x: 0, y: 0, z: 0 }
+        const torque = { x: 0, y: 0, z: 0 }
+        const impulseStr = 1 * delta
+        const torqueStr = 1 * delta
+
+        if (forward) {
+            impulse.z -= impulseStr
+            torque.x -= torqueStr
+        }
+        if (rightward) {
+            impulse.x += impulseStr
+            torque.z -= torqueStr
+        }
+        if (backward) {
+            impulse.z += impulseStr
+            torque.x += torqueStr
+        }
+        if (leftward) {
+            impulse.x -= impulseStr
+            torque.z += torqueStr
+        }
+
+
+
+        bodyRef.current.applyImpulse(impulse)
+        bodyRef.current.applyTorqueImpulse(torque)
+    })
     return (
         <RigidBody
+            ref={bodyRef}
             colliders='ball'
             position={[0, 1, 0]}
             restitution={0.2}
