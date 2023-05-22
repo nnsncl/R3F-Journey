82eff88d2e99d0cf38122748cba421594eb7f937
nnsncl <nuniroland@gmail.com>
2023-05-22 16:13:58 +0200

    Add Level boundaries

--- src/resources/12-Game/Level.jsx
+++ src/resources/12-Game/Level.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import * as THREE from 'three'
-import { RigidBody } from "@react-three/rapier"
+import { RigidBody, CuboidCollider } from "@react-three/rapier"
 import { useFrame } from "@react-three/fiber"
 import { useGLTF } from "@react-three/drei"
 
@@ -187,7 +187,40 @@ export const BlockEnd = ({ position = [0, 0, 0] }) => {
         </group>
     )
 }
+const Boundaries = ({ length = 1 }) => {
+    return (
+        <RigidBody type="fixed" restitution={0.2} friction={0}>
+            <mesh
+                castShadow
+                geometry={boxGeometry}
+                material={materials.wall}
+                scale={[0.3, 1.5, length * 4]}
+                position={[2.15, 0.7, -(length * 2) + 2]}
+            />
+            <mesh
+                receiveShadow
+                geometry={boxGeometry}
+                material={materials.wall}
+                scale={[0.3, 1.5, length * 4]}
+                position={[-2.15, 0.7, -(length * 2) + 2]}
+            />
+            <mesh
+                castShadow
+                geometry={boxGeometry}
+                material={materials.wall}
+                scale={[4.6, 1.4, 0.3]}
+                position={[0, 0.75, -(length * 4) + 2]}
+            />
 
+            <CuboidCollider
+                args={[2, 0.1, length * 2]}
+                position={[0, -0.1, -(length * 2) + 2]}
+                restitution={0.2}
+                friction={1}
+            />
+        </RigidBody>
+    )
+}
 export const Level = ({
     count = 5,
     types = [BlockSpinner, BlockLimbo, BlockAxe]
@@ -213,6 +246,7 @@ export const Level = ({
                 />
             )}
             <BlockEnd position={[0, 0, -(count + 1) * 4]} />
+            <Boundaries length={count + 2} />
         </React.Fragment>
     );
 };
