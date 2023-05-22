d99e95793d653a432ac1e80939e0a61a85a47233
nnsncl <nuniroland@gmail.com>
2023-05-22 12:55:53 +0200

    Add Spinner element and Rigid bodies

--- src/resources/12-Game/Level.jsx
+++ src/resources/12-Game/Level.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import * as THREE from 'three'
-
+import { RigidBody } from "@react-three/rapier"
+import { useFrame } from "@react-three/fiber"
 
 THREE.ColorManagement.legacyMode = false
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
@@ -11,25 +12,50 @@ const materials = {
     wall: new THREE.MeshStandardMaterial({ color: 'slategrey' }),
 }
 
+
+const SpinnerTrap = () => {
+    const spinnerRef = React.useRef()
+
+    useFrame((state) => {
+        const time = state.clock.getElapsedTime()
+        const eulerRotation = new THREE.Euler(0, time * 3, 0)
+        const quaternionRotation = new THREE.Quaternion()
+
+        quaternionRotation.setFromEuler(eulerRotation)
+        spinnerRef.current.setNextKinematicRotation(quaternionRotation)
+    })
+    return (
+        <RigidBody ref={spinnerRef} type='kinematicPosition' friction={0}>
+            <mesh castShadow scale={[0.4, 0.4, 3]} position={[0, 0.21, 0]}>
+                <boxGeometry />
+                <meshStandardMaterial color='ivory' />
+            </mesh>
+        </RigidBody>
+    )
+}
+
 const BlockStart = ({ position = [0, 0, 0] }) => {
 
     return (
-        <group position={position} >
-            {/* Floor */}
-            <mesh
-                receiveShadow
-                geometry={boxGeometry}
-                material={materials.floor01}
-                position={[0, -0.1, 0]}
-                scale={[4, 0.2, 4]}
-            />
-        </group>
+        <RigidBody type='fixed' >
+            <group position={position} >
+                {/* Floor */}
+                <mesh
+                    receiveShadow
+                    geometry={boxGeometry}
+                    material={materials.floor01}
+                    position={[0, -0.1, 0]}
+                    scale={[4, 0.2, 4]}
+                />
+            </group>
+        </RigidBody>
     )
 }
 export const Level = () => {
     return (
         <React.Fragment>
             <BlockStart position={[0, 0, 0]} />
+            <SpinnerTrap />
         </React.Fragment>
     );
 };
