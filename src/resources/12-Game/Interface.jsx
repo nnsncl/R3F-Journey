ee52c736d87c471bf3c71c26bf82866c4c8ed8a7
nnsncl <nuniroland@gmail.com>
2023-05-23 19:16:45 +0200

    Retrieve and calc elapsed time to display it into the UI

--- src/resources/12-Game/Interface.jsx
+++ src/resources/12-Game/Interface.jsx
@@ -1,24 +1,50 @@
 import React from "react"
+import { addEffect } from "@react-three/fiber"
 import { useKeyboardControls } from "@react-three/drei"
 import useGame from "./stores/useGame"
 
 export const Interface = () => {
+    const restart = useGame((state) => state.restart)
+    const phase = useGame((state) => state.phase)
+
     const forward = useKeyboardControls((state) => state.forward)
     const rightward = useKeyboardControls((state) => state.rightward)
     const backward = useKeyboardControls((state) => state.backward)
     const leftward = useKeyboardControls((state) => state.leftward)
     const jump = useKeyboardControls((state) => state.jump)
 
-    const restart = useGame((state) => state.restart)
-    const phase = useGame((state) => state.phase)
+    const timerRef = React.useRef()
 
-    console.log(phase)
+    React.useEffect(() => {
+        const unsubscribeEffect = addEffect(() => {
+            const state = useGame.getState()
+            let elapsedTime = 0
+
+            if (state.phase === "playing")
+                elapsedTime = Date.now() - state.startTime
+            else if (state.phase === "ended")
+                elapsedTime = state.endTime - state.startTime
+
+            elapsedTime = elapsedTime / 1000
+            elapsedTime = elapsedTime.toFixed(2)
+
+            if (timerRef.current)
+                timerRef.current.textContent = elapsedTime
+        })
+
+        return () => {
+            unsubscribeEffect()
+        }
+    }, [])
+
     return (
         <section className="interface" >
-            <div className="time">00</div>
+            <div ref={timerRef} className="time" />
+
             {phase === 'ended' && (
                 <div className="restart" onClick={restart} >Restart</div>
             )}
+
             <div className="controls">
                 <div className="raw">
                     <div className={`${forward ? 'active' : ''} key`} />
