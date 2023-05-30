b643fde2f344d568668a1c36f86784e594344c24
nnsncl <nuniroland@gmail.com>
2023-05-30 21:47:50 +0200

    add fragment / vertex shader base with pnoise funcs

---
+++ src/resources/15-Shaders/shaders/torusFragment.glsl
@@ -0,0 +1,10 @@
+varying vec3 vPosition;
+varying vec2 vUv;
+varying vec3 vNormal;
+varying vec3 vPattern;
+
+uniform float uTime;
+
+void main() {
+  gl_FragColor = vec4(vec3(1.0 , 1.0, 1.0) , 1.0);
+}
