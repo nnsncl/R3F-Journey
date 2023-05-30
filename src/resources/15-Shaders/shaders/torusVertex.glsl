023f1e58fc211294426cab0d6830145487637b00
nnsncl <nuniroland@gmail.com>
2023-05-30 22:59:05 +0200

    animate shader, tweak algorithms

--- src/resources/15-Shaders/shaders/torusVertex.glsl
+++ src/resources/15-Shaders/shaders/torusVertex.glsl
@@ -1,6 +1,7 @@
 varying vec3 vPosition;
 varying vec3 vNormal;
 varying vec2 vUv;
+varying vec3 vPattern;
 
 uniform float uTime;
 
@@ -45,11 +46,13 @@ void main() {
     vPosition = position;
     vNormal = normal;
 
-    float noiseMultiplier = clamp((abs(vUv.x - 0.5) - 0.3) * PI, 0.0, 1.0);
-    float noise = pnoise(vPosition);
+    float noiseMultiplier = clamp((abs(vUv.y - 0.5) - sin(vNormal.z * uTime)) * PI, 0.25, 0.5);
+    float noise = pnoise(vPosition * sin(uTime * 0.42));
 
     float displacement = noise * noiseMultiplier;
     vec3 newPosition = vPosition + vNormal * displacement;
 
     gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
+
+    vPattern = vec3(noiseMultiplier);
 }
