3630c86f6c1681ec7aac0f0a6b2d5364d67a5c15
nnsncl <nuniroland@gmail.com>
2023-05-31 08:43:25 +0200

    Tweak frag shader and scene, add post processing

--- src/resources/15-Shaders/Experience.jsx
+++ src/resources/15-Shaders/Experience.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import { DoubleSide } from "three";
 
 import { extend, useFrame } from '@react-three/fiber'
-import { OrbitControls, shaderMaterial } from "@react-three/drei";
+import { Environment, Float, PresentationControls, shaderMaterial } from "@react-three/drei";
 import {
-    SSR,
     EffectComposer,
-    Vignette,
-    Glitch,
     Noise,
     Bloom,
     DepthOfField
@@ -25,29 +22,50 @@ extend({ NoiseMaterial })
 
 export const Experience = () => {
     const shaderRef = React.useRef()
+    const shaderShape = React.useRef()
 
     useFrame((_, delta) => {
         shaderRef.current.uTime += delta
+        shaderShape.current.rotation.y += Math.PI * (delta * 0.1);
     })
 
     return (
         <React.Fragment>
             <color args={['#0E0E0E']} attach='background' />
-            <OrbitControls makeDefault />
 
             <EffectComposer multisampling={8} >
                 <Bloom mipmapBlur intensity={10} luminanceThreshold={0.2} />
+                <DepthOfField focusDistance={0.03} focalLength={0.03} bokehScale={10} />
                 <Noise premultiply />
             </EffectComposer>
 
-            <mesh position={[0, 0, 0]} >
-                <torusGeometry args={[1, 0.3, 100, 100]} />
-                <noiseMaterial
-                    ref={shaderRef}
-                    side={DoubleSide}
-                // wireframe
-                />
-            </mesh>
+            <Environment preset="city" resolution={16} />
+
+            <directionalLight position={[1, 2, 3]} intensity={1.5} />
+            <ambientLight intensity={0.5} />
+
+            <PresentationControls
+                global
+                config={{ mass: 2, tension: 2000 }}
+                snap={{ mass: 4, tension: 2000 }}
+                polar={[-0.2, 0.2]}
+                azimuth={[-Math.PI / 3, Math.PI / 3]}
+            >
+                <group ref={shaderShape} >
+                    <Float rotationIntensity={2} speed={2} >
+                        <mesh position={[0, 0, 0]} scale={0.6} >
+                            <torusGeometry args={[1, 0.3, 256, 256]} />
+                            <noiseMaterial ref={shaderRef} side={DoubleSide} />
+                        </mesh>
+                    </Float>
+                </group>
+
+                <mesh position={[0, -2, 0]} rotation={[-0.2, 0, 0]}>
+                    <cylinderGeometry />
+                    <meshStandardMaterial color={'#0E0E0E'} metalness={1} roughness={0.1} />
+                </mesh>
+            </PresentationControls>
+
         </React.Fragment>
     );
 };
