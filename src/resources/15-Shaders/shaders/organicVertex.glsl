6759770ee570d2e499b9602aced9cad1411a0655
nnsncl <nuniroland@gmail.com>
2023-05-31 19:54:25 +0200

    Add organic shader base

---
+++ src/resources/15-Shaders/shaders/organicVertex.glsl
@@ -0,0 +1,15 @@
+uniform float uTime;
+
+varying vec3 vPosition;
+varying vec3 vNormal;
+varying vec2 vUv;
+
+void main() {
+    vPosition = position;
+    vNormal = normal;
+    vUv = uv;
+
+    vec4 modelViewPosition = modelViewMatrix * vec4( position, 1.0 );
+    vec4 projectedPosition = projectionMatrix * modelViewPosition;
+    gl_Position = projectedPosition;
+}
