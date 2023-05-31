1b84f77bb20fb2d1fc414fcad664a21160bbe95e
nnsncl <nuniroland@gmail.com>
2023-05-31 20:01:02 +0200

    Animate fractal

--- src/resources/15-Shaders/shaders/organicFragment.glsl
+++ src/resources/15-Shaders/shaders/organicFragment.glsl
@@ -6,6 +6,7 @@ varying vec2 vUv;
 
 void main() {
     vec2 uv = vUv;
+    uv.y += uTime * 0.1;
 
-    gl_FragColor = vec4(vec3(step(0.5, fract(uv.x * 10.0))), 1);
+    gl_FragColor = vec4(vec3(step(0.5, fract(uv.y * 10.0))), 1);
 }
