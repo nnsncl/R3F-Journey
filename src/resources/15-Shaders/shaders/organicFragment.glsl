2ae2d2d4598c1e85d40d24d399d25ce938350ac0
nnsncl <nuniroland@gmail.com>
2023-06-01 21:18:08 +0200

    apply displacement to the vertex shader

--- src/resources/15-Shaders/shaders/organicFragment.glsl
+++ src/resources/15-Shaders/shaders/organicFragment.glsl
@@ -3,7 +3,8 @@ uniform float uTime;
 varying vec3 vPosition;
 varying vec3 vNormal;
 varying vec2 vUv;
+varying float vDisplacement;
 
 void main() {
-    gl_FragColor = vec4(vec3(1.0), 1.0);
+    gl_FragColor = vec4(vec3(vDisplacement), 1.0);
 }
