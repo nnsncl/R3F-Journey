befcbe14618bc1f5b2e4af21822104d35c995664
nnsncl <nuniroland@gmail.com>
2023-06-03 00:27:20 +0200

    Asjut rotation based on cursor pos

--- src/resources/15-Shaders/Experience.jsx
+++ src/resources/15-Shaders/Experience.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { extend, useFrame } from '@react-three/fiber'
-import { Environment, Float, PresentationControls, shaderMaterial } from "@react-three/drei";
+import { Environment, Float, shaderMaterial } from "@react-three/drei";
 import {
     EffectComposer,
     Noise,
@@ -23,9 +23,9 @@ export const Experience = () => {
     const noiseShaderRef = React.useRef()
     const shaderShape = React.useRef()
 
-    useFrame((_, delta) => {
+    useFrame((state, delta) => {
         noiseShaderRef.current.uTime += delta
-        shaderShape.current.rotation.y += Math.PI * (delta * 0.05);
+        shaderShape.current.rotation.y += Math.PI * (delta * state.mouse.x)
     })
 
     return (
@@ -42,26 +42,18 @@ export const Experience = () => {
                 <Noise opacity={0.05} />
             </EffectComposer>
 
-            <PresentationControls
-                global
-                config={{ mass: 2, tension: 2000 }}
-                snap={{ mass: 4, tension: 2000 }}
-                polar={[-0.2, 0.2]}
-                azimuth={[-Math.PI / 3, Math.PI / 3]}
-            >
-                <group ref={shaderShape} >
-                    <Float floatIntensity={2} speed={2} >
-                        <mesh position={[0, 0, 0]} scale={1} rotation={[Math.PI * 0.5, Math.PI * 0.25, 0]}>
-                            <torusGeometry args={[1, 0.2, 256, 256]} />
-                            <noiseMaterial ref={noiseShaderRef} />
-                        </mesh>
-                    </Float>
-                </group>
-                <mesh position={[0, -2, 0]} >
-                    <cylinderGeometry />
-                    <meshStandardMaterial color={'#000000'} metalness={1} roughness={0.2} />
-                </mesh>
-            </PresentationControls>
+            <group ref={shaderShape} >
+                <Float floatIntensity={2} speed={2} >
+                    <mesh position={[0, 0, 0]} scale={1} rotation={[Math.PI * 0.5, Math.PI * 0.25, 0]}>
+                        <torusGeometry args={[1, 0.2, 256, 256]} />
+                        <noiseMaterial ref={noiseShaderRef} />
+                    </mesh>
+                </Float>
+            </group>
+            <mesh position={[0, -2, 0]} >
+                <cylinderGeometry />
+                <meshStandardMaterial color={'#000000'} metalness={1} roughness={0.2} />
+            </mesh>
 
         </React.Fragment>
     );
